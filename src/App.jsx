d9cfc6c7b0a5f4a788e7341b849be7fbb1f88d1c
Kristjan Punno <k.punno@gmail.com>
2025-06-09 21:14:13 -0400

    add regions

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import { provider, auth, signInWithPopup, signOut } from "./firebase";
+import { getDocument } from "pdfjs-dist";
 import "./App.css";
 
 function App() {
+  //#region variables
   const [user, setUser] = useState(null);
+  //#endregion
 
-  const handleLogin = async() => {
+  //#region functions
+  // auth
+  const handleLogin = async () => {
     try {
       const result = await signInWithPopup(auth, provider);
       setUser(result.user);
@@ -13,17 +18,21 @@ function App() {
       // Remove console logs, need better logger (pino ??)
       console.error("Login failed", err);
     }
-  }
-
-  const handleLogout = async() => {
+  };
+  const handleLogout = async () => {
     await signOut(auth);
     setUser(null);
-  }
+  };
 
-  return(
-  <div> 
-    {
-      user? (
+  //pdf
+  // ... <object></object>
+  
+  //#endregion
+
+  //#region jsx
+  return (
+    <div>
+      {user ? (
         <div>
           <p>{user.displayName}</p>
           <button onClick={handleLogout}>Sign out</button>
@@ -32,10 +41,10 @@ function App() {
         <div>
           <button onClick={handleLogin}>Sign in</button>
         </div>
-      )
-    }
-  </div>
+      )}
+    </div>
   );
+  //#endregion
 }
 
 export default App;
